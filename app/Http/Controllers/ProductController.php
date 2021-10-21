55eb90fbb13ed393f52216fa40704456a20b0cbf
nhinovaio <47347404+nhinovaio@users.noreply.github.com>
2021-10-21 09:52:54 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -226,7 +226,7 @@ class ProductController extends BaseController
 
     function import_product(Request $request){
       if ($request->hasFile('file_upload')) {
-        Excel::toArray(new ProductImport, $request->file('file_upload'));
+        Excel::import(new ProductImport, $request->file('file_upload'));
       }
       return redirect()->back()->with('status', 'Nhập khẩu thành công');
     }
